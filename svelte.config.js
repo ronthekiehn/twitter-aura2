119640c1572030857212f083c9fbf4b3210f1956
Your Name <51499585+ronthekiehn@users.noreply.github.com>
2025-04-04 12:09:44 -0500

    runes??

--- svelte.config.js
+++ svelte.config.js
@@ -4,4 +4,5 @@ export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  compileOptions: {runes: false}
 }
