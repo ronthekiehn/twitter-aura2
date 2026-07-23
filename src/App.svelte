61e12589371a001235bd65d1dffaffb9819a59b6
Your Name <51499585+ronthekiehn@users.noreply.github.com>
2026-07-23 12:58:56 -0700

    Fix landing elements to viewport

--- src/App.svelte
+++ src/App.svelte
@@ -249,7 +249,7 @@
   }
 </script>
 
-<main class="flex w-full max-w-full flex-col items-center justify-end overflow-x-hidden min-h-screen text-center p-4 m-auto">
+<main class="flex w-full max-w-full min-h-screen flex-col items-center justify-end overflow-x-hidden p-4 text-center m-auto">
   <div class="fixed flex flex-col items-start top-2 left-2">
 
     <div class='flex my-1 sm:my-2'>
@@ -302,13 +302,13 @@
   {/if}
 
   {#if currentUser === null && !loading}
-    <div class="relative w-full h-[calc(100dvh-2rem)] overflow-hidden">
-    <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-white rounded-3xl shadow-lg border-4 border-black z-10 md:p-12 p-8 flex flex-col items-center w-full min-w-72 sm:max-w-md">
+    <div class="fixed inset-0 overflow-hidden">
+    <div class="absolute inset-x-4 top-1/2 -translate-y-1/2 mx-auto bg-white rounded-3xl shadow-lg border-4 border-black z-10 md:p-12 p-8 flex flex-col items-center min-w-72 max-w-md">
       <h1 class="text-lg md:text-2xl">WHAT COLOR IS YOUR AURA</h1>
       <TwitterInput bind:username on:submit={handleSubmit} />
     </div>
 
-    <div class="absolute inset-x-0 bottom-0 flex w-full flex-col items-center">
+    <div class="absolute inset-x-4 bottom-4 z-10 flex flex-col items-center">
     <button
       class="my-1 md:my-4 p-1 md:p-2 bg-white border-black shadow-md border-4 text-black rounded-lg hover:bg-slate-100 transition-colors text-sm md:text-base hover:shadow-lg hover:translate-y-[-2px]"
       on:click={() => showLeaderboard = !showLeaderboard}
